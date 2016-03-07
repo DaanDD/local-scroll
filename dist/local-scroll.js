9ee51071fc6980f1cfaf339a7e522aac61667b8a
Daan De Deckere <daan@sumocoders.be>
2016-03-07 15:02:13 +0100

    Replace webpack with simple Babel transpilation through Gulp

--- dist/local-scroll.js
+++ dist/local-scroll.js
@@ -1,103 +1,54 @@
-/******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
+'use strict';
 
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId])
-/******/ 			return installedModules[moduleId].exports;
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			exports: {},
-/******/ 			id: moduleId,
-/******/ 			loaded: false
-/******/ 		};
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+var LocalScroll = function () {
+  function LocalScroll() {
+    var options = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
 
-/******/ 		// Flag the module as loaded
-/******/ 		module.loaded = true;
+    _classCallCheck(this, LocalScroll);
 
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
+    this.options = Object.assign({
+      selector: "[data-scroll]",
+      offset: 0,
+      duration: 400
+    }, options);
+    this.addClickHandlers();
+  }
 
+  _createClass(LocalScroll, [{
+    key: 'addClickHandlers',
+    value: function addClickHandlers() {
+      var _this = this;
 
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
+      $(this.options.selector).on('click', function (e) {
+        e.preventDefault();
+        var href = $(e.target).attr('href');
 
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
+        if (href === '#') {
+          return _this.scrollTo(0);
+        } else {
+          var scrollToElem = $(href);
+          return scrollToElem.length && _this.scrollTo(scrollToElem.offset().top);
+        }
+      });
+    }
+  }, {
+    key: 'scrollTo',
+    value: function scrollTo(scrollY) {
+      $('html, body').animate({
+        scrollTop: scrollY + this.options.offset
+      }, this.options.duration);
+    }
+  }]);
 
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
+  return LocalScroll;
+}();
 
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(0);
-/******/ })
-/************************************************************************/
-/******/ ([
-/* 0 */
-/***/ function(module, exports) {
-
-	'use strict';
-
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
-
-	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-	var LocalScroll = function () {
-	  function LocalScroll() {
-	    var options = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
-
-	    _classCallCheck(this, LocalScroll);
-
-	    this.options = Object.assign({
-	      selector: "[data-scroll]",
-	      offset: 0,
-	      duration: 400
-	    }, options);
-	    this.addClickHandlers();
-	  }
-
-	  _createClass(LocalScroll, [{
-	    key: 'addClickHandlers',
-	    value: function addClickHandlers() {
-	      var _this = this;
-
-	      $(this.options.selector).on('click', function (e) {
-	        e.preventDefault();
-	        var href = $(e.target).attr('href');
-
-	        if (href === '#') {
-	          return _this.scrollTo(0);
-	        } else {
-	          var scrollToElem = $(href);
-	          return scrollToElem.length && _this.scrollTo(scrollToElem.offset().top);
-	        }
-	      });
-	    }
-	  }, {
-	    key: 'scrollTo',
-	    value: function scrollTo(scrollY) {
-	      $('html, body').animate({
-	        scrollTop: scrollY + this.options.offset
-	      }, this.options.duration);
-	    }
-	  }]);
-
-	  return LocalScroll;
-	}();
-
-	exports.default = LocalScroll;
-
-/***/ }
-/******/ ]);
+exports.default = LocalScroll;
